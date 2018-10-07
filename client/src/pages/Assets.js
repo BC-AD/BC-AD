0bc47c03f68c6b9bbc7034f821797a6efc8482c6
Val <dgm97@cornell.edu>
2018-10-07 10:25:37 -0700

    remove unneeded comments

--- client/src/pages/Assets.js
+++ client/src/pages/Assets.js
@@ -2,16 +2,6 @@ import React, { Component } from 'react';
 import assetAbi from '../contracts/asset.json';
 const web3 = window.web3 && new window.Web3(window.web3.currentProvider);
 
-
-// auth 0x9020316f378adfD6c0e7D700ca87DFdFa9F5C49b
-//
-// 0x335b018382cf360246692d03bfd3490bd45ea162
-
-
-// asset
-// 0x440d90178040ec558d27c23237a315c61238e9ed
-// 0x1c5D4218053dBC8B6fBc78C39EF25b21EF8Cc0eF
-
 class Assets extends Component {
   state = {
     assetAddress: '0x440d90178040ec558d27c23237a315c61238e9ed',
@@ -27,6 +17,7 @@ class Assets extends Component {
   getAssets = () => {
     const AssetInstance = this._getContract(assetAbi, this.state.assetAddress);
     AssetInstance.getAllAssets.call((err, data) => {
+      console.log(data);
       this.setState({
         assets: data
       });
